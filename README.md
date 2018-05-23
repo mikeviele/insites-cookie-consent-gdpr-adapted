bd7ea1ee7f754cfd9eedad4688b580ec920ed141
Michael Viele <mike@axionmedialab.com>
2018-05-22 21:10:13 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+## What is does this adaptation of Insites Cookie Consent do? 
+
+Lets start by looking at what Cookie Consent by Insites actually is...
+
+[Cookie Consent](https://cookieconsent.insites.com/) is a lightweight JavaScript plugin developed by Insites for alerting users about the use of cookies on your website.
+
+It is designed to help you comply with the EU's GDPR regulations, but out of the box it misses the mark with a true revokable consent. It also forces you to implement that EU compliance across the board putting valuable tracking data at risk in countries that do not require this compliance.
+
+I have taken the initial work done by Insites and adapted if for a better GDPR implementation. As I commit and ask for community feedback, this repository will also evolve in to something thats hope useful to people trying to comply.
+
+I am not a lawyer, nor is this legal advice in any way shape or form. This is merely my take, based on the GDPR rules, of a cookie consent that satisfies the GDPR's need for the ability to allow opt-in and out in perpetuity for users visiting your site from the EU. It will also counter that functionality with a more informational version of the consent for all countries outside of the EU.
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var minifyJS = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var deleteDirs = require('del');
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+var buildFolder = './build';
+var jsBuildFiles = [
+  './src/cc-gdpr-adapted.js'
+];
+var cssBuildFiles = [
+  // defined explicitly so they are combined in order
+  './src/styles/animation.css',
+  './src/styles/base.css',
+  './src/styles/layout.css',
+  './src/styles/media.css',
+
+  // all theme files
+  './src/styles/themes/*.css',
+];
+
+
+gulp.task('cleanup:begin', function () {
+  return deleteDirs([buildFolder]);
+});
+
+gulp.task('minify:js', function () {
+  return gulp.src(jsBuildFiles)            // get files
+    .pipe(minifyJS())                      // minify them
+    .pipe(concat('cc-gdpr-adapted.min.js'))  // combine them
+    .pipe(gulp.dest(buildFolder));          // save under a new name
+});
+
+gulp.task('minify:css', function () {
+  return gulp.src(cssBuildFiles)            // get files
+    .pipe(autoprefixer({browsers: ['IE 10', 'last 2 versions']}))
+    .pipe(minifyCSS())                      // minify them
+    .pipe(concat('cc-gdpr-adapted.min.css'))  // combine them
+    .pipe(gulp.dest(buildFolder));          // save under a new name
+});
+
+gulp.task('build', function(callback) {
+  return runSequence('cleanup:begin', 'minify:js', 'minify:css', callback);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(cssBuildFiles.concat(jsBuildFiles), ['build']);
+});
+
+function _minify(opts) {
+}
